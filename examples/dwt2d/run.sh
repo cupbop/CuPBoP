9152feb24f8a5ce29f31d168d41e882f69e23a73
Ruobing Han <hanruobing@gatech.edu>
2022-09-15 11:31:58 -0400

    remove useless examples

--- examples/dwt2d/run.sh
+++
@@ -1,8 +0,0 @@
-./dwt2d 4.bmp z.dwt -d 4x4 -f -5 -l 3
-# ./dwt2d 8.bmp -d 8x8 -f -5 -l 3
-# ./dwt2d 16.bmp -d 16x16 -f -5 -l 3
-# ./dwt2d 64.bmp -d 64x64 -f -5 -l 3
-
-# ./dwt2d 192.bmp -d 192x192 -f -5 -l 3
-# ls
-# ./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3
--- examples/dwt2d/run_cpu.sh
+++
@@ -1,7 +0,0 @@
-# ./dwt2d 192.bmp -d 192x192 -f -5 -l 3
-# ls
-# ./dwt2d rgb.bmp -d 1024x1024 -f -5 -l 3
-# ./dwt2d 16.bmp -d 16x16 -f -9 -l 3\
-./dwt2d 4.bmp  -d 4x4 -r -5 -l 3
-# ./dwt2d 4.bmp  -d 4x4 -r -9 -l 3
-# ./dwt2d 8.bmp  -d 8x8 -f -9 -l 3
